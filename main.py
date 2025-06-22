2a5f23b75bfa6ceabd53463e05fc67d15c073739
João Pedro Dandolini <jp.dandolini@gmail.com>
2025-06-22 05:02:17 -0300

    Versão inicial fucionar branch and bound (está errado)

--- main.py
+++ main.py
@@ -1,30 +1,16 @@
 from src.utils.parser import parse_file_to_problem
-from src.lp_solver.simplex import SimplexSolver
+from src.milp_solver.branch_and_bound import MILPSolver # <<<<< NOVA IMPORTAÇÃO
 
 def run():
-    print("--- Iniciando o Solver MILP ---")
-    problem_file_path = "data/exemplo_simples.txt"
-    problem = parse_file_to_problem(problem_file_path)
+    problem = parse_file_to_problem("data/exemplo_simples.txt")
     
-    print("--- Modelo Matemático Original ---")
-    print(problem)
-    print("--------------------------------\n")
+    # Cria uma instância do nosso solver principal
+    milp_solver = MILPSolver(problem)
     
-    solver = SimplexSolver(problem)
-    solution = solver.solve()
+    # Chama o método para resolver o problema MILP
+    solution = milp_solver.solve()
     
-    # --- IMPRESSÃO FINAL DOS RESULTADOS ---
-    print("\n--- Resumo da Solução (Relaxação LP) ---")
-    if solution and solution["status"] == "Optimal":
-        print(f"Status: {solution['status']}")
-        print(f"Valor da Função Objetivo: {solution['objective_value']:.4f}")
-        print("\nValores das Variáveis:")
-        for var, value in solution["variables"].items():
-            if value > 1e-6: # Só imprime variáveis com valor não-zero
-                print(f"  {var} = {value:.4f}")
-    else:
-        print(f"Não foi possível encontrar uma solução ótima. Status: {solution.get('status', 'Unknown')}")
-    print("----------------------------------------")
+    # No futuro, imprimiremos a solução final aqui...
 
 if __name__ == "__main__":
     run()
